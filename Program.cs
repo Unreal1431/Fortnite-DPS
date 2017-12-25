e54b465d9490301dacc5b87503dda29e6a7ee6c5
Unreal1431 <32221399+Unreal1431@users.noreply.github.com>
2017-12-26 03:34:32 +0400

    26.12.17
    rfref

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApplication12
     {
         static void Main(string[] args)
         {
-            // создал стрингу которая читает файл, и рандомизатор чисел...
+            // создал стрингу которая читает файл, и рандомизатор чисел.
             StreamReader objReader = new StreamReader("e:\\dps.txt");
             string dpsLine = ""; ArrayList arrText = new ArrayList();
             Random rnd = new Random();
@@ -33,34 +33,51 @@ namespace ConsoleApplication12
 
             dpsLine = objReader.ReadLine();
             double speed = double.Parse(dpsLine);
-            Console.WriteLine("Скорость атаки: {0} \n", speed);
+            Console.WriteLine("Скорость атаки: {0}", speed);
 
-            // умножение скорости атаки, потому что дробные числа это хуёво.
-            speed = speed * 100;
+            dpsLine = objReader.ReadLine();
+            int AmmoSize = int.Parse(dpsLine);
+            Console.WriteLine("Обойма: {0}", AmmoSize);
+
+            dpsLine = objReader.ReadLine();
+            double Reloadspeed = double.Parse(dpsLine);
+            Console.WriteLine("Скорость перезарядки: {0} \n", Reloadspeed);
+
+            // умножение скорости атаки и обоймы, потому что дробные числа это хуёво.
+            speed *= 100;
+            AmmoSize *= 100;
             double mid_dps = 0;
+            int AmmoSpend = 0;
 
+            // повторить атаки в течении 1000 секунд 100 раз для большей точности.
             for (int y = 0; y < 100; y++)
             {
                 double total = 0;
                 double dps = 0;
-                // атаковать в течении 100 секунд
-                for (int i = 0; i < 100; i++)
-                    // выполнить количество атак в секунду равное скорости атаки
-                    for (int j = 0; j < speed; j++)
-                    {// проверка на срабатывание крита, если крит сработал прибавить к урону критический урон
-                        if (rnd.Next(0, 101) <= cc && cc != 0)
-                            total = total + (dmg * cdmg);
-                        // если крит не сработал - обычный урон который плюсуется в общий урон.
-                        else
-                            total = total + dmg;
+                // атаковать в течении 1000 секунд
+                for (double i = 0; i < 1000; i++)
+                {
+                    // если кол-во потраченых пуль превышает обойму - прибавить к времени атаки, время перезарядки с нулевым уроном.
+                    if (AmmoSize < AmmoSpend)
+                    {
+                        i += Reloadspeed;
+                        AmmoSpend -= AmmoSize;
                     }
-                // общий урон разделить на 100 , потому что атаки проводились 100 секунд, и еще на 100, потому что умножали скорость.
-                dps = (total / 100) / 100;
-                mid_dps = mid_dps + dps;
-
+                    // выполнить количество атак в секунду равное скорости атаки
+                    else for (int j = 0; j < speed; j++)
+                        {// проверка на срабатывание крита, если крит сработал прибавить к урону критический урон, если не сработал - обычный урон.
+                            total += (rnd.Next(0, 101) <= cc && cc != 0)? (dmg * cdmg): dmg;
+                            // прибавить кол-во потраченых пуль.
+                                AmmoSpend++;
+                        }
+                }
+                // общий урон разделить на 1000 , потому что атаки проводились 1000 секунд, и еще на 100, потому что умножали скорость.
+                dps = (total / 1000) / 100;
+                mid_dps += dps;
             }
-            mid_dps = mid_dps / 100;
-            Console.WriteLine("ДПС = {0}",mid_dps);
+           // вычисляем и выводим на экран средний дпс путём деления на 100, так как проводили атаки 100 раз.
+            mid_dps /= 100;
+            Console.WriteLine("ДПС = {0}", mid_dps);
 
 
 
